fc9a4fb8d09f530876504427db2b70ec7b0b2062
Delattre1 <daniieeldelattre@gmail.com>
2022-09-14 12:04:20 -0300

    Added movimentation to the player

--- Assets/Scripts/Enemies/DarkNinjaController.cs
+++ Assets/Scripts/Enemies/DarkNinjaController.cs
@@ -2,16 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// REF https://www.youtube.com/watch?v=dHkbDn-KQ9E
 public class DarkNinjaController : MonoBehaviour
 {
     Rigidbody2D rb2d;
+    [SerializeField] Transform castPos;
+    Vector3 baseScale;
+
+    string facingDirection;
+    [SerializeField] float baseCastDistance;
+    const string _L = "left";
+    const string _R = "right";
     float vel = 5;
+
+
     void Start()
     {
+        facingDirection = _R;
+        baseScale = transform.localScale;
         rb2d = GetComponent<Rigidbody2D>();
     }
 
     void FixedUpdate() {
         rb2d.velocity = new Vector2(vel, rb2d.velocity.y);
+
+        if (isHittingWall()) {print("HIT WALL!");}
     }
+
+    bool isHittingWall() {
+        bool  isHitting = false;
+        float castDist = baseCastDistance;
+
+        // Define if should cast the distance to LEFT or RIGHT
+        if (facingDirection == _L) {castDist = -baseCastDistance;}
+        
+        // Determine target destination based on cast distance
+        Vector3 targetPos = castPos.position;
+        targetPos.x += castDist;
+
+        isHitting = Physics2D.Linecast(castPos.transform.position, 
+                                        targetPos, 
+                                        1 << LayerMask.NameToLayer("Ground"));
+        return isHitting;
+    }
+
 }
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Animator controlAnimation;   
+    Rigidbody2D rb2D;
+    Vector3 direction, currentPosition;
+    float yAxis, xAxis;
+    bool isMoving;
+    float vel = 10;
+
+    void Start() {
+        controlAnimation = GetComponent<Animator>();
+    }
+
+    void Update() {
+        xAxis = Input.GetAxis("Horizontal");
+        yAxis = Input.GetAxis("Vertical");
+        direction = new Vector3(xAxis, yAxis, 0);
+
+        //Update animation
+        isMoving = direction.magnitude != 0;
+        controlAnimation.SetBool("isMoving", isMoving);
+    }
+
+    void FixedUpdate() { //padrao execucao a cada 0.2 segundos
+        rb2D = GetComponent<Rigidbody2D>();
+        currentPosition = rb2D.position;
+
+        rb2D.MovePosition(
+            currentPosition + direction * vel * Time.deltaTime
+        );
+
+        //aimRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //Debug.DrawRay(aimRay.origin, aimRay.direction*100, Color.red);
+
+        //if (Physics.Raycast(aimRay, out impact, 100, GroundMask)) {
+        //    positionPlayerAim = impact.point - transform.position;
+        //    positionPlayerAim.y = transform.position.y;
+
+        //    Quaternion newRotation = Quaternion.LookRotation(positionPlayerAim);
+        //    rb2D.MoveRotation(newRotation);
+        //}
+    }
+}
